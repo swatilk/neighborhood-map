c89c2b51ef6bf6434a4a84c82d227382f552c857
Swati Kamtar <kamtar.swati@gmail.com>
2018-01-06 11:58:43 -0800

    Add lines for JSHint to know about node

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,83 @@
-'use strict'
+/* jshint node: true */
 
-var ngrok = require('ngrok');
+(function () {
+  "use strict";
 
-module.exports = function(grunt) {
+  var ngrok = require('ngrok');
 
-// Load grunt tasks
+  module.exports = function(grunt) {
 
-require('load-grunt-tasks')(grunt);
+  // Load grunt tasks
 
-// Grunt configuration
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-  clean: {
-        dev: {
-          src: ['dist']
-        },
-  },
-  uglify: {
-      my_target: {
+  require('load-grunt-tasks')(grunt);
+
+  // Grunt configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+          dev: {
+            src: ['dist']
+          },
+    },
+    uglify: {
+        my_target: {
+          files: [{
+              expand: true,
+              cwd: 'js',
+              src: '*.js',
+              dest: 'dist/js',
+              ext: '.min.js'
+          }]
+        }
+    },
+    cssmin: {
+      target: {
         files: [{
-            expand: true,
-            cwd: 'js',
-            src: '*.js',
-            dest: 'dist/js',
-            ext: '.min.js'
+          expand: true,
+          cwd: 'css',
+          src: '*.css',
+          dest: 'dist/css',
+          ext: '.min.css'
         }]
       }
-  },
-  cssmin: {
-    target: {
-      files: [{
-        expand: true,
-        cwd: 'css',
-        src: '*.css',
-        dest: 'dist/css',
-        ext: '.min.css'
-      }]
-    }
-  },
-  pagespeed: {
-    options: {
-      nokey: true,
-      locale: "en_GB",
-      threshold: 20
     },
-    local: {
+    pagespeed: {
       options: {
-        strategy: "desktop"
-      }
-    },
-    mobile: {
-      options: {
-        strategy: "mobile"
+        nokey: true,
+        locale: "en_GB",
+        threshold: 20
+      },
+      local: {
+        options: {
+          strategy: "desktop"
+        }
+      },
+      mobile: {
+        options: {
+          strategy: "mobile"
+        }
       }
     }
-  }
 
-});
-
-  // Register customer task for ngrok
-  grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
-    var done = this.async();
-    var port = 8080;
-
-    ngrok.connect(port, function(err, url) {
-      if (err !== null) {
-        grunt.fail.fatal(err);
-        return done();
-      }
-      grunt.config.set('pagespeed.options.url', url);
-      grunt.task.run('pagespeed');
-      done();
-    });
   });
 
-  // Register default tasks
-  grunt.registerTask('default', ['clean', 'uglify','cssmin', 'psi-ngrok']);
-}
+    // Register customer task for ngrok
+    grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
+      var done = this.async();
+      var port = 8080;
+
+      ngrok.connect(port, function(err, url) {
+        if (err !== null) {
+          grunt.fail.fatal(err);
+          return done();
+        }
+        grunt.config.set('pagespeed.options.url', url);
+        grunt.task.run('pagespeed');
+        done();
+      });
+    });
+
+    // Register default tasks
+    grunt.registerTask('default', ['clean', 'uglify','cssmin', 'psi-ngrok']);
+  };
+}());
